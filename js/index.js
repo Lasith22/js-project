6a0107f21a0419391ccaca4ed4d42878f9fd4e2c
lasith herath <lasithherath00@gmail.com>
2025-02-21 18:54:49 +0530

    refactor: reorganize JavaScript and CSS files, removing old versions and adding new implementations

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
-var a = 7;
-console.log(a);
+// function isPalidrom(number) {
+//   const temp = number.toString();
+
+//   return number === temp.reverse.join();
+// }
+
+let fruits = [1, 3, 5, 4, 3, 4, 5, 6, 3, 5, 6, 5];
+
+fruits.sort((a, b) => a - b);
+console.log(fruits);
